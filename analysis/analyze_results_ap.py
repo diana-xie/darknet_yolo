b633c0cd1b59edf8a2671c5fbf813661d084810a
Diana <diana.lynn.xie@gmail.com>
2020-07-11 07:16:43 -0400

    Added evaluation metrics analysis

--- analysis/analyze_results_ap.py
+++ analysis/analyze_results_ap.py
@@ -2,58 +2,152 @@ import os
 import re
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import logging
 
-files = [f for f in os.listdir('ap') if f.endswith('.txt')]
-results = []
+# params
+COST_FP_WEED = 0.3  # Cost incurred, when incorrectly killing 1 corn plant
+COST_TP_WEED = 0.5  # Cost saved, when correctly killing 1 weed plant
 
-for filename in files:
+# setup
+FILES = [f for f in os.listdir('ap') if f.endswith('.txt')]  # files with all the data
+DATA_REGEX = {'iou_threshold': "mAP@(\d+\.\d+)",
+              'conf_threshold': "for conf_thresh = (\d+\.\d+), precision",
+              'ap_corn': "name = corn, ap = (\d+\.\d+)",  # average precision, corn
+              'ap_weed': "name = weed, ap = (\d+\.\d+)",
+              'tp_corn': "name = corn.+\(TP = (\d+)",  # true positives
+              'tp_weed': "name = weed.+\(TP = (\d+)",
+              'fp_corn': "name = corn.+FP = (\d+)",  # false positives
+              'fp_weed': "name = weed.+FP = (\d+)",
+              'average_iou': "average IoU.+ (\d+\.\d+)",
+              'm_ap': "mean average precision.+ = (\d+\.\d+)",
+              'precision': ", precision = (\d+\.\d+),",
+              'recall': ", recall = (\d+\.\d+), ",
+              'f1': "F1-score = (\d+\.\d+), ",
+              'iou_average': "average IoU = (\d+\.\d+), "
+              }
 
-    try:
-        f = open('ap/' + filename, "r")
-        text = f.read()
-    except Exception as ex:
-        logging.error("Error reading file '{}': {}".format(filename, ex))
-        raise ex
 
-    # get data
-    try:
-        data = {'iou_threshold': re.compile(r"mAP@(\d+\.\d+)").findall(text)[0],
-                'ap_corn': re.compile(r"name = corn, ap = (\d+\.\d+)").findall(text)[0],  # average precision, corn
-                'ap_weed': re.compile(r"name = weed, ap = (\d+\.\d+)").findall(text)[0],
-                'tp_corn': re.compile(r"name = corn.+\(TP = (\d+)").findall(text)[0],  # true positives
-                'tp_weed': re.compile(r"name = weed.+\(TP = (\d+)").findall(text)[0],
-                'fp_corn': re.compile(r"name = corn.+FP = (\d+)").findall(text)[0],  # false positives
-                'fp_weed': re.compile(r"name = weed.+FP = (\d+)").findall(text)[0],
-                'average_iou': re.compile(r"average IoU.+ (\d+\.\d+)").findall(text)[0],
-                'm_ap': re.compile(r"mean average precision.+ = (\d+\.\d+)").findall(text)[0]}
-        results.append(data)
-    except Exception as ex:
-        logging.error("Error getting data from file '{}': {}".format(filename, ex))
-        raise ex
+def extract_data(FILES: list):
+    """
+    extract the data from text files
+    Parameters
+    ----------
+    FILES: list of files
 
-# convert to numeric
-df_ap = pd.DataFrame(results).astype(float)
-df_ap[['ap_corn', 'ap_weed', 'average_iou']] = df_ap[['ap_corn', 'ap_weed', 'average_iou']]/100  # % to decimal
-df_ap = df_ap.sort_values('iou_threshold')
+    Returns
+    -------
 
-# plot - ap
-fig = plt.figure()
-plt.title("Average Precision in Test Set".format(filename))
-plt.plot(df_ap['iou_threshold'], df_ap['ap_weed'], "-r", label='Weed (AP)')
-plt.plot(df_ap['iou_threshold'], df_ap['ap_corn'], "-b", label='Corn (AP)')
-plt.xlabel("IoU Threshold")
-plt.ylabel("Average Precision (AP)")
-plt.legend(loc="lower right")
-fig.savefig('AP_analysis.png')
+    """
+    results = []
+    for filename in FILES:
+        # open data file
+        try:
+            f = open('results_ap_conf_iou/' + filename, "r")
+            text = f.read()
+        except Exception as ex:
+            logging.error("Error reading file '{}': {}".format(filename, ex))
+            raise ex
+        # extract data
+        for variable_name, variable_regex in DATA_REGEX.items():
+            data = {}
+            try:
+                data[variable_name] = re.compile(variable_regex).findall(text)[0]
+            except Exception as ex:
+                logging.error("Error getting data from file '{}': {}".format(filename, ex))
+                raise ex
+            results.append(data)
+    df_results = pd.DataFrame(results).astype(float)
+    return df_results
 
-# plot - fp weeds
-fig = plt.figure()
-plt.title("False Positives (Weeds) in Test Set".format(filename))
-plt.plot(df_ap['iou_threshold'], df_ap['fp_weed'], "-g", label='Weed (FP)')
-plt.plot(df_ap['iou_threshold'], df_ap['fp_corn'], "-y", label='Corn (FP)')
-plt.xlabel("IoU Threshold")
-plt.ylabel("# of False Positives (FP)")
-plt.legend(loc="lower right")
-fig.savefig('FP_analysis.png')
+
+def convert_to_numeric(df_results: pd.DataFrame):
+    """
+    Converts data from results, to numeric. Also performs scaling
+    Parameters
+    ----------
+    df_results: dataframe of results, which were extracted from txt files in extract_data()
+
+    Returns
+    -------
+
+    """
+    # % to decimal
+    df_results[['ap_corn', 'ap_weed', 'average_iou']] = df_results[['ap_corn', 'ap_weed', 'average_iou']] / 100
+    # df_results = df_results.sort_values('iou_threshold') # sort rows by iou_threshold
+    return df_results
+
+
+def compute_cost(df_results: pd.DataFrame):
+    """
+    compute "cost" score
+    Parameters
+    ----------
+    df_results
+
+    Returns
+    -------
+
+    """
+    df_results["cost_score"] = (COST_TP_WEED * df_results["tp_weed"]) - (COST_FP_WEED * df_results["fp_weed"])
+    return df_results
+
+
+def generate_plots(df_results: pd.DataFrame):
+    """
+    Generate plots of results
+    Parameters
+    ----------
+    df_results
+
+    Returns
+    -------
+
+    """
+    # plot - ap
+    fig = plt.figure()
+    plt.title("Average Precision in Test Set - {}".format(filename))
+    plt.plot(df_results['iou_threshold'], df_results['ap_weed'], "-r", label='Weed (AP)')
+    plt.plot(df_results['iou_threshold'], df_results['ap_corn'], "-b", label='Corn (AP)')
+    plt.plot(df_results['iou_threshold'], df_results['m_ap'], "-g", label='mAP (across both classes)')
+    plt.xlabel("IoU Threshold")
+    plt.ylabel("Average Precision (AP)")
+    plt.legend(loc="lower right")
+    fig.savefig('MAP.png')
+
+    # plot - fp weeds
+    fig = plt.figure()
+    plt.title("False Positives (Weeds) in Test Set - {}".format(filename))
+    plt.plot(df_results['iou_threshold'], df_results['fp_weed'], "-b", label='Weed (FP)')
+    plt.plot(df_results['iou_threshold'], df_results['fp_corn'], "-r", label='Corn (FP)')
+    plt.xlabel("IoU Threshold")
+    plt.ylabel("# of False Positives (FP)")
+    # plot - tp weeds (on same fig)
+    plt.plot(df_results['iou_threshold'], df_results['tp_weed'], "-o", color="b", label='Weed (TP)')
+    plt.plot(df_results['iou_threshold'], df_results['tp_corn'], "-o", color="r", label='Corn (TP)')
+    plt.legend(loc="lower right")
+    fig.savefig('TPFP.png')
+
+    # PR curve
+    fig = plt.figure()
+    plt.title("Precision-Recall Curve across different IoU thresholds - \n for filename {}".format(filename))
+    plt.plot(df_results['recall'], df_results['precision'])
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    fig.savefig('Precision-Recall.png')
+
+    # Recall-IoU curve
+    fig = plt.figure()
+    plt.title("Recall-IoU Curve - \n for filename {}".format(filename))
+    plt.plot(df_results['iou_threshold'], df_results['recall'], "-b", label="Recall")
+    plt.plot(df_results['iou_threshold'], df_results['precision'], "-r", label="Precision")
+    plt.xlabel("IoU threshold")
+    plt.legend(loc="lower right")
+    fig.savefig('Recall-Precision-IoU.png')
+
+    # Cost curve
+    fig = plt.figure()
+    plt.title("Cost - \n for filename {}".format(filename))
+    plt.plot(df_results['iou_threshold'], df_results['cost_score'], "-b", label="cost")
+    plt.xlabel("IoU threshold")
+    plt.legend(loc="lower right")
+    fig.savefig('Recall-Precision-IoU.png')
--- analysis/analyze_results_fps.py
+++ analysis/analyze_results_fps.py
@@ -3,37 +3,30 @@ import re
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import cv2
 
-# iou_threshold = 24
-# filename = "result_iouthresh_{}.txt".format(str(iou_threshold))
-files = [f for f in os.listdir() if f.endswith('.txt')]
+files = [f for f in os.listdir('fps') if f.endswith('.txt')]
 
 for filename in files:
 
-    f = open(filename, "r")
+    f = open('fps/' + filename, "r")
     text = f.read()
 
-    # make FPS table
-    df_fps = pd.DataFrame(re.compile('AVG_FPS:(\d+\.\d+)').findall(text))  # get avg_fps col
-    df_fps['fps'] = re.compile(r'\nFPS:(\d+\.\d+)').findall(text)  # get fps col
-
-    # pre-processing
-    df_fps = df_fps.astype(float)
-    df_fps = df_fps[df_fps['fps'] != 0]
-    df_fps = df_fps.rename(columns={0: 'avg_fps'}).reset_index()
+    # get FPS
+    df_fps = pd.DataFrame(re.compile('AVG_FPS:(\d+\.\d+)').findall(text)).rename(columns={0: 'avg_fps'})  # avg fps
+    df_fps['fps'] = re.compile(r'\nFPS:(\d+\.\d+)').findall(text)  # fps
+    df_fps = df_fps.astype(float)  # convert to float
+    df_fps = df_fps[df_fps['fps'] != 0]  # remove invalid entries
     df_fps.rename(columns={'index': 'idx'}, inplace=True)
 
-    # get cumulative mean
+    # get FPS, cumulative mean
     df_fps['cummean_raw'] = df_fps['fps'].expanding().mean()  # raw cumulative mean
     warmup_cutoff = df_fps['cummean_raw'].mean() - 2*df_fps['cummean_raw'].std()  # remove warm-up values, which lowers FPS
     df_fps['cummean_filtered'] = np.nan
     df_fps['cummean_filtered'][df_fps['fps'] > warmup_cutoff] = \
         df_fps['fps'][df_fps['fps'] > warmup_cutoff].expanding().mean()
 
-    # get total number of frames
-
-
-    # plot
+    # plot FPS, analysis
     fig = plt.figure()
     plt.title("FPS in '{}'".format(filename))
     plt.plot(df_fps['idx'], df_fps['avg_fps'], "-b", label='FPS (mean)')
@@ -44,3 +37,23 @@ for filename in files:
     plt.ylabel("FPS or average FPS")
     plt.legend(loc="lower right")
     fig.savefig('FPS_analysis_{}.png'.format(filename))
+
+    # get actual number of frames & duration of video
+    """https://stackoverflow.com/questions/49048111/how-to-get-the-duration-of-video-using-cv2"""
+    cap = cv2.VideoCapture("2019_07_24_1_Up_Crash.MP4")
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print("num frames: {}".format(num_frames))
+    fps = cap.get(cv2.CAP_PROP_FPS)  # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
+    print("fps: {}".format(fps))
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count / fps
+    print('fps = ' + str(fps))
+    print('number of frames = ' + str(frame_count))
+    print('duration (S) = ' + str(duration))
+    minutes = int(duration / 60)
+    seconds = duration % 60
+    print('duration (M:S) = ' + str(minutes) + ':' + str(seconds))
+    cap.release()
+
+
+    # get confidence
